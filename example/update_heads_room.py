c579e0beb7e62073eaf3dd9fa8712c7d4e7c4f20
Thong Nguyen <nxthongbk@gmail.com>
2020-06-11 11:51:29 +0700

    update

---
+++ example/update_heads_room.py
@@ -0,0 +1,13 @@
+import requests
+
+server = 'http://119.82.135.244:3000/'
+path ='heads'
+
+def update_room(room_name, room_head_count):
+    data = {'room':  room_name,
+        'count': room_head_count
+        }
+    r = requests.post('http://119.82.135.244:3000/heads', data = data)
+    
+if __name__=="__main__":
+    update_room('room1',12)
